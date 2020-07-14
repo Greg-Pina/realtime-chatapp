e39eec086db64f1056b4e7a5de86733a1d41c979
Greg Pina <pinagm2825@gmail.com>
2020-07-13 21:13:01 -0400

    testing chatform logging

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
 const socket = io()
+const chatForm = document.getElementById('chat-form')
 
 socket.on('message', (message) => {
 	console.log(message)
+})
+
+// Message submit
+chatForm.addEventListener('submit', (e) => {
+	e.preventDefault()
+	const msg = e.target.elements.msg.value
+	console.log(msg)
 })
