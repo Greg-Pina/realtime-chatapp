6ae3e623898a43d6a0c55aac92e1dfb008325799
Greg Pina <pinagm2825@gmail.com>
2020-07-13 21:33:57 -0400

    turning messages into generic objects and passing them around

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
+const formatMessage = require('./utils/messages')
 const socketio = require('socket.io')
 
 const app = express()
@@ -10,22 +11,27 @@ const io = socketio(server)
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+const botName = 'ChatBot'
+
 // Run when client connects
 io.on('connection', (socket) => {
 	// Welcome current user
-	socket.emit('message', 'Welcome to the chatroom')
+	socket.emit('message', formatMessage(botName, 'Welcome to the chatroom'))
 
 	// Broadcast when a user connects
-	socket.broadcast.emit('message', 'A user has joined the chat')
+	socket.broadcast.emit(
+		'message',
+		formatMessage(botName, 'A user has joined the chat')
+	)
 
 	// Runs when client disconnects
 	socket.on('disconnect', () => {
-		io.emit('message', 'A user has left the chat')
+		io.emit('message', formatMessage(botName, 'A user has left the chat'))
 	})
 
 	// Listen for chat message
 	socket.on('chatMessage', (msg) => {
-		io.emit('message', msg)
+		io.emit('message', formatMessage('USER', msg))
 	})
 })
 
